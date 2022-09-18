bcef45726e3bdd45292046eac1bc4e7dce54b07d
fuyu-quant <ulti4929@gmail.com>
2022-09-18 20:49:15 +0900

    a

--- sample.py
+++ sample.py
@@ -3,7 +3,8 @@ from omegaconf import DictConfig
 import hydra
 @hydra.main(config_path=".",config_name="config.yaml")
 def main(cfg: DictConfig) -> None:
-    print(cfg.pretty())
+    #print(cfg.pretty())
+    print(cfg)
 
 
 if __name__ == '__main__':
