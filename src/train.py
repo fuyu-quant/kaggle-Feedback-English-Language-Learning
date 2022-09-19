f7c472db705322a76999b10db702e713f417a4e6
fuyu-quant <ulti4929@gmail.com>
2022-09-19 21:40:23 +0900

    a

--- src/train.py
+++ src/train.py
@@ -400,8 +400,7 @@ def valid_fn(cfg, model, dataloader):
         score = validation_loss / valid_size
 
         if cfg.setting.use_tqdm:
-            vbar.set_description('Batch loss: {:.4f}'.format(score))
-            vbar.close()
+            print(vbar.set_description('Batch loss: {:.4f}'.format(score)))
 
         #bar.set_postfix(Valid_Loss=epoch_v_loss)
         torch.cuda.empty_cache()
