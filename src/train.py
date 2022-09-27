99edcd75ef225d6d6eeb4839be9761c96c7bf525
toma tanaka <ulti4929@gmail.com>
2022-09-28 01:55:36 +0900

    a

--- src/train.py
+++ src/train.py
@@ -329,7 +329,7 @@ def train_fn(cfg, model, train_dataloader, optimizer, epoch, scheduler,
                 torch.save(model.state_dict(), model_path)
                 print("Model Saved")
          
-    return global_step, loss, total_samples
+    return global_step, loss, total_samples, best_score
 
 
 
@@ -468,13 +468,14 @@ def training_loop(cfg, fold):
     global_step = 0
 
     for epoch in range(1, cfg.setting.num_epochs + 1): 
-        g_step, epoch_loss, t_samples = train_fn(cfg, model, train_dataloader, optimizer, epoch, scheduler, 
+        g_step, epoch_loss, t_samples, b_score = train_fn(cfg, model, train_dataloader, optimizer, epoch, scheduler, 
                                 valid_dataloader, fold, tra_len, loss, total_samples, global_step,
-                                best_score = np.inf)
+                                best_score)
         global_step = g_step
         loss = epoch_loss
         total_samples = t_samples
-    del model, optimizer, scheduler
+        best_score = b_score
+
     torch.cuda.empty_cache()
     gc.collect()
     
